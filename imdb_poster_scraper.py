95625f9c8a332669410ccc5e074fe7b15f5fd37c
GauthamN3004 <gauthamn3004@gmail.com>
2020-06-18 14:09:25 +0530

    cleaning the dataset

--- imdb_poster_scraper.py
+++ imdb_poster_scraper.py
@@ -3,7 +3,10 @@ from bs4 import BeautifulSoup
 import requests
 
 def get_poster(imdb_id):
-    url = "https://www.imdb.com/title/tt"+str(imdb_id)+"/"
+    imdb_id = str(imdb_id)
+    while len(imdb_id)<7:
+        imdb_id = '0'+imdb_id
+    url = "https://www.imdb.com/title/tt"+imdb_id+"/"
     req = requests.get(url).text
     soup = BeautifulSoup(req,"lxml")
     for div in soup.find_all("div",class_ = "poster"):
